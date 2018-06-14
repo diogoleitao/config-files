56a277d8a6fd08b936214fa635025a24913a311e
Diogo Leitão <diogo.n.leitao@gmail.com>
2018-06-14 18:52:22 +0100

    Fixed gottafetchemall function to use built-in cd

--- shell/functions.sh
+++ shell/functions.sh
@@ -3,12 +3,12 @@ gottafetchemall() {
   echo "Gotta fetch'em all...!"
   for D in */; do
     echo "Gotcha! ${D} was caught!"
-    cd "${D}"
+    builtin cd "${D}"
     if [ -d ".git/" ]; then
       echo -e "\tWhat? ${D} is evolving!"
       git fetch --all --prune
     fi
-    cd ../
+    builtin cd ../
     echo "Got away safely."
     echo
   done
