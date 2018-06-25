e57e4dcbc02aca3c62a8acf7943b3fdcd1c045de
Diogo Leitão <diogo.n.leitao@gmail.com>
2018-06-25 12:30:31 +0100

    Added activenv alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,5 +1,6 @@
-alias cntdirs='ls -l | grep ^d | wc -l'
-alias cntfiles='ls -l | grep ^- | wc -l'
+alias cntdirs="ls -l | grep ^d | wc -l"
+alias cntfiles="ls -l | grep ^- | wc -l"
 alias gulog="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
-alias ping='ping -c 5'
+alias ping="ping -c 5"
 alias reload_zsh="source ~/.zshrc"
+alias activenv="source venv/bin/activate"
