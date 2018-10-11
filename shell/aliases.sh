8a1710587ac01e82afcbea09583ac328fc37ed51
Diogo Leitão <d.leitao@runtime-revolution.com>
2018-10-11 12:48:58 +0100

    Added react-native-cli aliases and organized aliases file

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,12 +1,26 @@
-alias activenv="source venv/bin/activate"
 alias cntdirs="ls -l | grep ^d | wc -l"
 alias cntfiles="ls -l | grep ^- | wc -l"
-alias fab='python2 -m fabric'
-alias gps="git push staging $(git_current_branch):master -f"
-alias grs="git remote show"
-alias gulog="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
-alias htail='heroku logs --tail --remote'
 alias knock_knock="lsof -i"
 alias ping="ping -c 5"
 alias reload_zsh="source ~/.zshrc"
+
+# Git
+alias gps="git push staging $(git_current_branch):master -f"
+alias grs="git remote show"
+alias gulog="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
+
+# Heroku
+alias htail='heroku logs --tail --remote'
+
+# Mac
 alias reset-cam="sudo killall VDCAssistant && sudo killall AppleCameraAssistant"
+
+# Python
+alias activenv="source venv/bin/activate"
+alias fab='python2 -m fabric'
+
+# React-Native CLI
+alias rnand="react-native run-android"
+alias rnios="react-native run-ios"
+alias rnl="react-native link"
+alias rns="react-native start"
