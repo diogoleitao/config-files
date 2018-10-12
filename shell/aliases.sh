9676fa2f36e28d25d129557c2a549bc3c2a5d737
Diogo Leitão <d.leitao@runtime-revolution.com>
2018-10-12 14:46:02 +0100

    Added git stash alias (gs)

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,26 +1,27 @@
-alias cntdirs="ls -l | grep ^d | wc -l"
-alias cntfiles="ls -l | grep ^- | wc -l"
-alias knock_knock="lsof -i"
-alias ping="ping -c 5"
-alias reload_zsh="source ~/.zshrc"
+alias cntdirs='ls -l | grep ^d | wc -l'
+alias cntfiles='ls -l | grep ^- | wc -l'
+alias knock_knock='lsof -i'
+alias ping='ping -c 5'
+alias reload_zsh='source ~/.zshrc'
 
 # Git
-alias gps="git push staging $(git_current_branch):master -f"
-alias grs="git remote show"
-alias gulog="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
+alias gps='git push staging $(git_current_branch):master -f'
+alias grs='git remote show'
+alias gs='git stash'
+alias gulog='git log --pretty=format:"%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s" --date=short'
 
 # Heroku
 alias htail='heroku logs --tail --remote'
 
 # Mac
-alias reset-cam="sudo killall VDCAssistant && sudo killall AppleCameraAssistant"
+alias reset-cam='sudo killall VDCAssistant && sudo killall AppleCameraAssistant'
 
 # Python
-alias activenv="source venv/bin/activate"
+alias activenv='source venv/bin/activate'
 alias fab='python2 -m fabric'
 
 # React-Native CLI
-alias rnand="react-native run-android"
-alias rnios="react-native run-ios"
-alias rnl="react-native link"
-alias rns="react-native start"
+alias rnand='react-native run-android'
+alias rnios='react-native run-ios'
+alias rnl='react-native link'
+alias rns='react-native start'
