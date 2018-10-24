20941e45d938ffe6112830c1bb866380f72d0c17
Diogo Leitão <d.leitao@runtime-revolution.com>
2018-10-24 15:35:00 +0100

    Added 'git stash -p' and changed knock_knock to knock

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,6 +1,6 @@
 alias cntdirs='ls -l | grep ^d | wc -l'
 alias cntfiles='ls -l | grep ^- | wc -l'
-alias knock_knock='lsof -i'
+alias knock='lsof -i'
 alias ping='ping -c 5'
 alias reload_zsh='source ~/.zshrc'
 alias rmrf='rm -rf'
@@ -9,6 +9,7 @@ alias rmrf='rm -rf'
 alias gps='git push staging $(git_current_branch):master -f'
 alias grs='git remote show'
 alias gs='git stash'
+alias gsp='git stash -p'
 alias gulog='git log --pretty=format:"%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s" --date=short'
 
 # Heroku
