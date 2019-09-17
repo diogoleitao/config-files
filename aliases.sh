125df5b6a083fa393c64d0670b231e82575c9856
Diogo Leitão <d.leitao@runtime-revolution.com>
2019-09-17 17:58:38 +0100

    Removed python and react-native-cli aliases Reordered everything

--- aliases.sh
+++ aliases.sh
@@ -1,13 +1,24 @@
+##############################
+# Util
+## Development
+alias sourcenv='export $(cat .env | xargs)'
+
+## General
 alias cntdirs='ls -l | grep ^d | wc -l'
 alias cntfiles='ls -l | grep ^- | wc -l'
 alias knock='lsof -i'
-alias listen_to='kafkacat -b localhost:9092 -C -t'
 alias ping='ping -c 5'
 alias reload_zsh='source ~/.zshrc'
 alias rmrf='rm -rf'
-alias sourcenv='export $(cat .env | xargs)'
 
-# Docker
+## Mac
+alias be='bundle exec'
+alias listen_to='kafkacat -b localhost:9092 -C -t'
+alias reset-cam='sudo killall VDCAssistant && sudo killall AppleCameraAssistant'
+
+
+##############################
+# docker
 alias dkb='docker build'
 alias dkcl='docker container ls'
 alias dkil='docker image ls'
@@ -15,6 +26,8 @@ alias dklog='docker logs'
 alias dkps='docker ps'
 alias dkstop='docker stop'
 
+
+##############################
 # docker-compose
 alias dcb='docker-compose build'
 alias dcdo='docker-compose down'
@@ -25,13 +38,17 @@ alias dcstart='docker-compose start'
 alias dcstop='docker-compose stop'
 alias dcup='docker-compose up -d'
 
-# Git
+
+##############################
+# git
 alias grs='git remote show'
 alias gs='git stash'
 alias gsp='git stash -p'
 alias gulog='git log --pretty=format:"%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s" --date=short'
 alias gulc='git reset HEAD~'
 
+
+##############################
 # Heroku
 alias haa='heroku accounts:add'
 alias hac='heroku accounts:current'
@@ -39,18 +56,7 @@ alias hal='heroku accounts'
 alias har='heroku accounts:remove'
 alias has='heroku accounts:set'
 
-# Mac
-alias be='bundle exec'
-alias reset-cam='sudo killall VDCAssistant && sudo killall AppleCameraAssistant'
 
+##############################
 # NPM
 alias global_packages='npm list -g --depth 0'
-
-# Python
-alias fab='python2 -m fabric'
-
-# React-Native CLI
-alias rnand='react-native run-android'
-alias rnios='react-native run-ios'
-alias rnl='react-native link'
-alias rns='react-native start'
